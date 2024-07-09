22aaccef2ff09f988ae89ce454363a1f5b8e52ed
Dillon Raphael <dillon@creatorsneverdie.com>
2024-07-09 17:06:56 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Hono } from 'hono'
+import { getRandomInt } from './lib/helpers'
+
+const app = new Hono()
+
+app
+.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+.get('/random', (c) => {
+  return c.text(getRandomInt(1, 100).toString())
+})
+
+
+
+export default app
